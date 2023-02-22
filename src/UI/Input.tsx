b6289cff53c3d9cc42764abad364a5922cf7ad34
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-02-22 12:26:01 +0530

    Implemented basic form validation

--- src/UI/Input.tsx
+++ src/UI/Input.tsx
@@ -7,10 +7,11 @@ interface Props {
     id?: string;
   };
   ref: ForwardedRef<HTMLInputElement>;
+  className?: string;
 }
 
-const Input: React.FC<Props> = forwardRef(({ input }, ref) => {
-  return <input {...input} ref={ref} />;
+const Input: React.FC<Props> = forwardRef(({ input, className }, ref) => {
+  return <input {...input} ref={ref} className={className} />;
 });
 
 export default Input;
--- src/register/Register.tsx
+++ src/register/Register.tsx
@@ -1,21 +1,50 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, SetStateAction, useRef, useState } from "react";
 import Input from "../UI/Input";
+import { IRegister } from "../types/Register.types";
+import classes from "./Register.module.css";
+
+const isEmpty = (value: string) =>
+  value.trim() === "" && value.trim().length === 0;
 
 const Register = () => {
-  const nameInputRef = useRef<HTMLInputElement>(null);
-  const emailInputRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
-  const confirmPasswordRef = useRef<HTMLInputElement>(null);
+  const nameInputRef = useRef<HTMLInputElement | null>(null);
+  const emailInputRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+  const confirmPasswordRef = useRef<HTMLInputElement | null>(null);
+  const [formInputIsValid, setFormInputIsValid] = useState({
+    name: true,
+    email: true,
+    password: true,
+    confirmPassword: true,
+  });
+  const [data, setData] = useState<IRegister | null>(null);
   const submitRegisterFormHandler = (e: FormEvent) => {
     e.preventDefault();
-    const name = nameInputRef.current?.value;
-    const email = emailInputRef.current?.value;
-    const password = passwordRef.current?.value;
-    const confirmPassword = confirmPasswordRef.current?.value;
-    console.log(name);
-    console.log(email);
-    console.log(password);
-    console.log(confirmPassword);
+    const name = nameInputRef.current!.value;
+    const email = emailInputRef.current!.value;
+    const password = passwordRef.current!.value;
+    const confirmPassword = confirmPasswordRef.current!.value;
+    const nameIsValid = !isEmpty(name);
+    const emailIsValid = !isEmpty(email);
+    const passwordIsValid = !isEmpty(password);
+    const confirmPasswordIsValid = !isEmpty(confirmPassword);
+    const formIsValid =
+      nameIsValid && emailIsValid && passwordIsValid && confirmPasswordIsValid;
+    setFormInputIsValid({
+      name: nameIsValid,
+      email: emailIsValid,
+      password: passwordIsValid,
+      confirmPassword: confirmPasswordIsValid,
+    });
+    if (formIsValid) {
+      console.log("VALID");
+      setData({
+        name: name,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+      });
+    }
     nameInputRef.current!.value = "";
     emailInputRef.current!.value = "";
     passwordRef.current!.value = "";
@@ -28,25 +57,49 @@ const Register = () => {
           <Input
             ref={nameInputRef}
             input={{ type: "text", placeholder: "email" }}
+            className={!formInputIsValid.name ? classes.invalid : ""}
           />
+          {!formInputIsValid.name && (
+            <div className={classes["invalid-container"]}>
+              <p>Name can't be empty</p>
+            </div>
+          )}
         </div>
         <div>
           <Input
             ref={emailInputRef}
             input={{ type: "text", placeholder: "email" }}
+            className={!formInputIsValid.email ? classes.invalid : ""}
           />
+          {!formInputIsValid.email && (
+            <div className={classes["invalid-container"]}>
+              <p>Email can't be empty</p>
+            </div>
+          )}
         </div>
         <div>
           <Input
             ref={passwordRef}
             input={{ type: "password", placeholder: "Password" }}
+            className={!formInputIsValid.password ? classes.invalid : ""}
           />
+          {!formInputIsValid.password && (
+            <div className={classes["invalid-container"]}>
+              <p>Password can't be empty</p>
+            </div>
+          )}
         </div>
         <div>
           <Input
             ref={confirmPasswordRef}
             input={{ type: "password", placeholder: "Confirm Password" }}
+            className={!formInputIsValid.confirmPassword ? classes.invalid : ""}
           />
+          {!formInputIsValid.name && (
+            <div className={classes["invalid-container"]}>
+              <p>Confirm Password can't be empty</p>
+            </div>
+          )}
         </div>
         <div>
           <button type="submit">Submit</button>
