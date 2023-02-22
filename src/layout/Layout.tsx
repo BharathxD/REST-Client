ae354aea8f87815d08df4a790897ce5bac1f645c
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-02-22 12:36:06 +0530

    Added the Navigation Bar and the Footer to the Layout Component

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,8 +1,20 @@
-const Layout = () => {
-    return (
-        <>
-        </>
-    )
+import { FC, ReactNode } from "react";
+import { Fragment } from "react";
+import Footer from "./Footer/Footer";
+import Nav from "./Nav/Nav";
+
+interface ILayoutProps {
+  children: ReactNode;
 }
+
+const Layout: FC<ILayoutProps> = ({ children }) => {
+  return (
+    <Fragment>
+      <Nav />
+      {children}
+      <Footer />
+    </Fragment>
+  );
+};
 
 export default Layout;
